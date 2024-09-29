a558140ac474cac5c3343c8dfe6a1629b42674c6
giuse-pio <giupeppepio7@gmail.com>
2024-09-29 15:32:37 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/3_13.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "3_13.exe"
+  "3_13.exe.manifest"
+  "3_13.pdb"
+  "CMakeFiles/3_13.dir/main.c.obj"
+  "CMakeFiles/3_13.dir/main.c.obj.d"
+  "lib3_13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int g1,m1,a1,g2,m2,a2,diff;
+    printf("inserisci la prima data");
+    scanf("%d %d %d",&g1,&m1,&a1);
+    printf("inserisci la seconda data");
+    scanf("%d %d %d",&g2, &m2, &a2);
+    diff=(g1-g2) + (m1-m2)*30 + (a1-a2)*360;
+    if(diff<0){
+        printf("la seconda data e' la piu' recente");
+    } else {
+        printf("la prima data e' la piu' recente");
+    }
+}
